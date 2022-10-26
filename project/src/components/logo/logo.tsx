ae9d04486d1fe1a11275752ca47aa4e0b0decd44
Valentin <maksimov.valentin@gmail.com>
2022-10-26 15:22:49 +0300

    fix 3

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -1,7 +1,11 @@
-export default function Logo(): JSX.Element{
+type LogoProps = {
+  isLight?: boolean;
+}
+
+export default function Logo({isLight}: LogoProps): JSX.Element{
   return (
     <div className="logo">
-      <a className="logo__link">
+      <a className={`logo__link ${isLight ? 'logo__link--light' : ''}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
