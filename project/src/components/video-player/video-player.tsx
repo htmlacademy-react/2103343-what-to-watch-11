7b97eedb7a89bfe321edc1d2cfdca521e8430754
Valentin <Maksimov.valentin@gmail.com>
2022-11-08 00:54:12 +0300

    save 2 (add state to movie-card)

--- project/src/components/video-player/video-player.tsx
+++ project/src/components/video-player/video-player.tsx
@@ -2,11 +2,10 @@ import {FilmsType} from '../../types/types';
 
 type VideoPlayerProps = {
   movie: FilmsType;
-
 }
 
 export default function VideoPlayer (props: VideoPlayerProps): JSX.Element{
   return(
-    <video src={props.movie.previewVideoLink} poster={props.movie.previewImage}  muted={true}/>
+    <video src={props.movie.previewVideoLink} poster={props.movie.previewImage} muted autoPlay width="280" height="175"/>
   );
 }
