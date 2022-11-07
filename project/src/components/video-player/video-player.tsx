f9457977d498795ee042b392e7051a36dfe730d4
Valentin <maksimov.valentin@gmail.com>
2022-11-07 16:40:34 +0300

    save 1 player add

---
+++ project/src/components/video-player/video-player.tsx
@@ -0,0 +1,12 @@
+import {FilmsType} from '../../types/types';
+
+type VideoPlayerProps = {
+  movie: FilmsType;
+
+}
+
+export default function VideoPlayer (props: VideoPlayerProps): JSX.Element{
+  return(
+    <video src={props.movie.previewVideoLink} poster={props.movie.previewImage}  muted={true}/>
+  );
+}
