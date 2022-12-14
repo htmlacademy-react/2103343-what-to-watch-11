09aa03a5d50fce1cd0f33b0f3b4235ba74abcde7
Valentin <maksimov.valentin@gmail.com>
2022-12-14 17:25:04 +0300

    save 2.3

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -32,14 +32,14 @@ export const fetchFilmAction = createAsyncThunk<FilmType, string, {
   },
 );
 
-export const checkAuthAction = createAsyncThunk<void, undefined, {
+export const checkAuthAction = createAsyncThunk<UserData, undefined, {
   dispatch: AppDispatch;
   state: State;
   extra: AxiosInstance;
 }>(
   'user/checkAuth',
   async (_arg, { extra: api }) => {
-    const { data } = await api.get(APIRoute.Login);
+    const { data } = await api.get<UserData>(APIRoute.Login);
     return data;
   },
 );
